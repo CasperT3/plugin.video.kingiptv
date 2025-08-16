892b65f670881db93c36419c0470ef70bbce9e8e
Joel Silva <53149347+zoreu@users.noreply.github.com>
2025-08-16 19:43:43 -0300

    Update ClientScraper.py

--- lib/ClientScraper.py
+++ lib/ClientScraper.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0'
-PROXY = 'https://webproxy.liyao.blog/'
+PROXY = 'https://proxy.liyao.space/------'
 
 class cfscraper:
     session = requests.Session()
